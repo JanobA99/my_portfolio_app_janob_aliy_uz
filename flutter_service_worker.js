b559913d73b5b8203c44b5339c5677c3782105bb
Yulchiev Alijon <alijonyolchiyev@gmail.com>
2020-05-03 12:20:06 +0500

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/GoogleSansRegular.ttf": "000be6a5acfcd4e4a425e9f1bb6c80f0",
 "assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/pk.jpg": "0469c57dc43552de4c3e6350896af8a0",
+"assets/pk.jpg": "273600d9b115a3d27f411f926ce16d12",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
